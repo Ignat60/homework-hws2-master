73569543c6d9ba093a332702b3acedab0619185c
Ignat60 <dedaignat@gmail.com>
2024-04-15 22:51:45 +0300

    ready

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -9,17 +9,35 @@ export const homeWorkReducer = (
   action: ActionType
 ): UserType[] => {
   // need to fix any
-  debugger;
+  // debugger;
   switch (action.type) {
     case "sort": {
-      // by name
-
-      return state; // need to fix
+      const copiedState = [...state];
+      copiedState.sort((a: UserType, b: UserType) => {
+        // Проверяем направление сортировки
+        if (action.payload === "up") {
+          // Сортировка в порядке возрастания
+          if (a.name > b.name) return 1;
+          if (a.name < b.name) return -1;
+          return 0;
+        } else if (action.payload === "down") {
+          // Сортировка в порядке убывания
+          if (a.name > b.name) return -1;
+          if (a.name < b.name) return 1;
+          return 0;
+        }
+        // Возвращаем 0, если нет направления сортировки
+        return 0;
+      });
+      return copiedState;
     }
     case "check": {
-      return state; // need to fix
+      return state.filter((i) => i.age > action.payload);
+      // need to fix
     }
+
     default:
+      // Если action.type не соответствует ни одному из case, возвращаем текущее состояние
       return state;
   }
 };
